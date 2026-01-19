5f50f7d8d98d2f35e4cfe0c1fc65d3c93230a795
9insane9 <145199591+9insane9@users.noreply.github.com>
2026-01-19 14:47:36 +0200

    testing

--- part4/blog/api/blogApi.js
+++ part4/blog/api/blogApi.js
@@ -8,11 +8,57 @@ apiRouter.get("/blogs", async (req, res) => {
   res.json(blogs)
 })
 
+apiRouter.get("/blogs/:id", async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+
+    if (!blog) {
+      return res.status(404).json({ error: "blog not found" })
+    }
+
+    res.json(blog)
+  } catch (err) {
+    next(err)
+  }
+})
+
 apiRouter.post("/blogs", async (req, res, next) => {
   try {
     const savedBlog = await new Blog(req.body).save()
 
     res.status(201).json(savedBlog)
+  } catch (err) {
+    next(err)
+  }
+})
+
+apiRouter.delete("/blogs/:id", async (req, res, next) => {
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(req.params.id)
+
+    if (!deletedBlog) {
+      return res.status(404).json({ error: "blog not found" })
+    }
+    res.status(204).end()
+  } catch (err) {
+    next(err)
+  }
+})
+
+apiRouter.put("/blogs/:id", async (req, res, next) => {
+  try {
+    const { likes } = req.body
+
+    const blog = await Blog.findById(req.params.id)
+
+    if (!blog) {
+      return res.status(404).json({ error: "blog not found" })
+    }
+
+    blog.likes = likes
+
+    const updatedBlog = await blog.save()
+    res.json(updatedBlog)
   } catch (err) {
     next(err)
   }
--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -1,5 +1,5 @@
 const assert = require("node:assert")
-const { test, after, beforeEach } = require("node:test")
+const { test, after, beforeEach, describe } = require("node:test")
 const mongoose = require("mongoose")
 const Blog = require("../models/blog")
 const supertest = require("supertest")
@@ -9,73 +9,155 @@ const api = supertest(app)
 
 //  probably better to compare to data in db, not use initialBlogs
 //  like course suggests
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
 
-after(async () => {
-  await mongoose.connection.close()
-})
+describe("when there are some blogs saved", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+  after(async () => {
+    await mongoose.connection.close()
+  })
 
-test("blogs are returned as JSON", async () => {
-  const result = await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
-})
+  test("blogs are returned as JSON", async () => {
+    const result = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+  })
 
-test("correct amount of blogs", async () => {
-  const result = await api.get("/api/blogs")
-  assert.strictEqual(helper.initialBlogs.length, result.body.length)
-})
+  test("correct amount of blogs get added", async () => {
+    const result = await api.get("/api/blogs")
+    assert.strictEqual(helper.initialBlogs.length, result.body.length)
+  })
 
-test("id property is named 'id', not '_id'", async () => {
-  const result = await api.get("/api/blogs")
-  assert(result.body[0].id && !result.body[0]._id)
-})
+  test("can get specific blog based on id", async () => {
+    const testBlog = {
+      title: "specificTest",
+      author: "specificTest",
+      url: "specificTest",
+      likes: 0,
+    }
 
-test("blog gets posted successfully", async () => {
-  const testBlog = {
-    title: "postTest",
-    author: "postTest",
-    url: "postTest",
-    likes: 0,
-  }
+    const sentBlog = await api.post("/api/blogs/").send(testBlog).expect(201)
+    const requestedBlog = await api.get(`/api/blogs/${sentBlog.body.id}`)
 
-  const result = await api.post("/api/blogs").send(testBlog).expect(201)
-  const finalBlogs = await helper.blogsInDb()
+    assert.strictEqual(requestedBlog.body.title, sentBlog.body.title)
+  })
 
-  const finalAmount = finalBlogs.length
-  const initialAmount = helper.initialBlogs.length
+  describe("on creation...", async () => {
+    test("blog gets posted successfully", async () => {
+      const testBlog = {
+        title: "postTest",
+        author: "postTest",
+        url: "postTest",
+        likes: 0,
+      }
 
-  assert.strictEqual(finalAmount, initialAmount + 1)
-  assert(finalBlogs.map((b) => b.title).includes(testBlog.title))
-})
+      const initialBlogs = await helper.blogsInDb()
+      const result = await api.post("/api/blogs").send(testBlog).expect(201)
+      const finalBlogs = await helper.blogsInDb()
 
-test("likes default to zero if missing from request", async () => {
-  const testBlog = {
-    title: "likeTest",
-    author: "likeTest",
-    url: "likeTest",
-  }
+      const finalAmount = finalBlogs.length
+      const initialAmount = initialBlogs.length
 
-  const result = await api.post("/api/blogs").send(testBlog).expect(201)
+      assert.strictEqual(finalAmount, initialAmount + 1)
+      assert(finalBlogs.map((b) => b.title).includes(testBlog.title))
+    })
 
-  assert.strictEqual(result.body.likes, 0)
-})
+    test("id property is named 'id', not '_id'", async () => {
+      const result = await api.get("/api/blogs")
+      assert(result.body[0].id && !result.body[0]._id)
+    })
 
-test.only("server responds with 400 if request missing title or url", async () => {
-  const testBlogNoTitle = {
-    author: "likeTest",
-    url: "likeTest",
-  }
+    test("if parameter 'likes' missing from request, default to zero", async () => {
+      const testBlog = {
+        title: "likeTest",
+        author: "likeTest",
+        url: "likeTest",
+      }
 
-  const testBlogNoUrl = {
-    title: "likeTest",
-    author: "likeTest",
-  }
+      const result = await api.post("/api/blogs").send(testBlog).expect(201)
 
-  const result1 = await api.post("/api/blogs").send(testBlogNoTitle).expect(400)
-  const result2 = await api.post("/api/blogs").send(testBlogNoUrl).expect(400)
+      assert.strictEqual(result.body.likes, 0)
+    })
+
+    test("if parameter 'title' or 'url' missing from request, respond with status 400", async () => {
+      const testBlogNoTitle = {
+        author: "likeTest",
+        url: "likeTest",
+      }
+
+      const testBlogNoUrl = {
+        title: "likeTest",
+        author: "likeTest",
+      }
+
+      const result1 = await api
+        .post("/api/blogs")
+        .send(testBlogNoTitle)
+        .expect(400)
+      const result2 = await api
+        .post("/api/blogs")
+        .send(testBlogNoUrl)
+        .expect(400)
+    })
+  })
+
+  describe("on deletion...", async () => {
+    test("existing blog gets deleted correctly", async () => {
+      const testBlog = {
+        title: "deleteTest",
+        author: "deleteTest",
+        url: "deleteTest",
+        likes: 0,
+      }
+
+      const addedBlog = await api.post("/api/blogs").send(testBlog).expect(201)
+      const blogsAfterAdding = await helper.blogsInDb()
+      const id = addedBlog.body.id
+
+      const deletion = await api.delete(`/api/blogs/${id}`).expect(204)
+      const finalBlogs = await helper.blogsInDb()
+
+      assert.ok(blogsAfterAdding.some((b) => b.id === id))
+      assert.ok(!finalBlogs.some((b) => b.id === id))
+    })
+
+    test("invalid id reports responds with status 400", async () => {
+      await api
+        .delete("/api/blogs/inamedmychildafteraspellineldenring")
+        .expect(400)
+    })
+
+    test("if valid id, but blog does not exist in db, responds with status 404", async () => {
+      const validId = await helper.nonExistingId()
+
+      await api.delete(`/api/blogs/${validId}`).expect(404)
+    })
+  })
+
+  describe("on update...", async () => {
+    test("likes can be updated", async () => {
+      const newBlog = {
+        title: "updateTest",
+        author: "updateTest",
+        url: "updateTest",
+        likes: 0,
+      }
+
+      const created = await api.post("/api/blogs").send(newBlog).expect(201)
+
+      const updatedBlog = {
+        ...created.body,
+        likes: created.body.likes + 1,
+      }
+
+      const result = await api
+        .put(`/api/blogs/${created.body.id}`)
+        .send(updatedBlog)
+
+      assert.strictEqual(result.body.likes, 1)
+    })
+  })
 })
